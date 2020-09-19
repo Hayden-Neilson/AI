4f4c14257d41f735f6ddbe58b578d9fe625a277a
Hayden-Neilson <haydenneilson5@gmail.com>
2020-09-19 14:06:45 -0600

    more q learn

--- notes/notes.py
+++ notes/notes.py
@@ -1,3 +1,4 @@
 # learned the difference of v algorithum
 # and the differ btween q learning
 # learned about the temporal difference
+# learned about how regular q learning is not enough to do complex things
